8505863f0e7a1e51d18ac2cc6f2b42b4804a4546
Rahsun McAfee <rahsun@rahsunmcafee.com>
2016-05-01 13:35:02 -0400

    httpoison support

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule DiscordElixir.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    [applications: [:logger, :httpoison]]
   end
 
   # Dependencies can be Hex packages:
@@ -27,6 +27,9 @@ defmodule DiscordElixir.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    [{:credo, "~> 0.3", only: [:dev, :test]}]
+    [
+      {:httpoison, "~> 0.8.0"},
+      {:credo, "~> 0.3", only: [:dev, :test]}
+    ]
   end
 end
